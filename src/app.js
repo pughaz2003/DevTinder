a0af2090f947785c94d13ac091da67495a8a4de2
pughazhendhi <pughaz668@gmail.com>
2024-10-21 20:02:59 +0530

    routes get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,19 @@ const express = require("express");
 
 const app = express();
 
-
-
-app.use("/hello", (req, res) => {
-    res.send("hello from the another route!");
+app.delete("/user", (req, res) => {
+    res.send("deleted successfully!");
     });
 
-app.use("/test", (req, res) => {
+app.get("/user", (req, res) => {
+    res.send({name:'pughaz',age:'21'});
+    });
+
+app.post("/user", (req, res) => {
 res.send("hello from the server!");
 });
+
+
 
 app.listen(3000, () => {
     console.log("server is listenning on port 3000");
