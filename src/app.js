686c205777c8a6d9b55f402fc77269a50136ec76
pughazhendhi <pughaz668@gmail.com>
2024-11-25 13:08:41 +0530

    diffrent api

--- src/app.js
+++ src/app.js
@@ -18,7 +18,69 @@ try{
 }
 
 
+
+
 })
+app.get("/user",async(req,res)=>{
+
+  const userEmail = req.body.emailId;
+try{
+ 
+  const user = await User.find({emailId:userEmail}); 
+  if(user.length === 0){
+    res.status(404).send("something went wrong"); 
+  }else{
+      
+  res.send(user);
+
+  }
+}catch{
+  res.status(404).send("something went wrong");
+}
+  
+
+
+})
+
+app.get("/feed",async (req,res)=>{
+
+ const userAge =req.body.age;
+  try{
+  const users = await User.findOne({age:userAge});
+  res.send(users);
+}catch{
+    res.status(404).send("something went wrong");
+  }
+})
+
+app.delete("/users",async (req,res)=>{
+
+  const userAge =req.body.age;
+ 
+  try{
+  const users = await User.findOneAndDelete({age:userAge});
+  
+   res.status(200).send("ssuccessfully deleted"); 
+
+  
+ }catch{
+     res.status(404).send("something went wrong");
+   }
+ })
+
+ app.patch("/update",async (req,res)=>{
+
+  const userId =req.body._id;
+  const data = req.body
+   try{
+   const users = await User.findByIdAndUpdate({_id:userId},data);
+   res.send("user updated successfully");
+ }catch{
+     res.status(404).send("something went wrong");
+   }
+ })
+ 
+
 
 
 
