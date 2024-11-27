e07993bc5c2aad671f774ef2c4593ba52009b209
pughazhendhi <pughaz668@gmail.com>
2024-11-27 13:47:31 +0530

    different api

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
     firstName :
     {
        type: String,
+       required: true,
+       minLength:5,
+       
     },
     lastName :
     {
@@ -13,14 +16,36 @@ const userSchema = new mongoose.Schema({
     emailId :
     {
       type:String,
+      required:true,
+      index:true,
+      unique: true,
+      lowercase:true,
+      trim:true,
     },
     password:
     {
        type: Number,
+       required:true,
     },
     age:{
        type: Number,
-    }
+       min:18,
+    },
+    gender:{
+      type:String,
+      validate(value){
+         const allowedGender = ['male','female','others'];
+        if(!allowedGender.includes(value)) {
+    throw new Error("Invalid gender. Allowed values are 'male', 'female', or 'others'.");
+        }
+      }
+      
+    },
+   about:{
+      type:String,
+      default:"remind in all data",
+
+   }
 })
 
 module.exports = mongoose.model('User',userSchema);
